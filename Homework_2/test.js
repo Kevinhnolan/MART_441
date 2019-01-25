b16e3c99cceed04f375899fe1d8c903feb8da29e
kevin <kevinhnolan@github.com>
2019-01-25 15:47:29 -0500

    Update test.js

--- Homework_2/test.js
+++ Homework_2/test.js
@@ -1,3 +1,1 @@
-console.log("Three favorite restaurantes would have to be 1.Delayne's Pub in North Conway NH 2. The Parka Pub in Bartlett NH, and 3. Jamesbar Missoula MT"
-"Snack foods-Cookies, Bananas, Oranges "
-"Favorite fruits--Oranges is a must, along with Banana and Bell peppers!");
+console.log("Three favorite restaurantes would have to be 1.Delayne's Pub in North Conway NH 2. The Parka Pub in Bartlett NH, and 3. Jamesbar Missoula MT Snack foods-Cookies, Bananas, Oranges Favorite fruits--Oranges is a must, along with Banana and Bell peppers!");
